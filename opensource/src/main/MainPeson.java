7cdc6579bc6e2cb7c8940eba972ea1558559b2a4
mã đại hải <67916342+kieungochai@users.noreply.github.com>
2020-07-16 14:37:40 +0700

    section09

--- opensource/src/main/MainPeson.java
+++ opensource/src/main/MainPeson.java
@@ -7,60 +7,63 @@ import com.trungtamjava.model.Train;
 import com.trungtamjava.model.car;
 
 public class MainPeson {
-		public static void main(String[] args) {
-			while(true) {
-				System.out.println(" 1 : nhap thong tin ve xe");
-				System.out.println("2 : nhap thong tin ve tau");
-				System.out.println("3 : hien thi thong tin ve xe");
-				System.out.println("4 : exit");
-				
-				System.out.println("chon thong tin");
-				Scanner sc = new Scanner(System.in);
-				int select = sc.nextInt();
-				
-				if(select == 1 ) {
-					Person p = new car();
-					input(p);
-				}else if(select == 2) {
-					Person p = new Train();
-					input(p);
-				}else if(select == 3) {
-					Person p = new Person();
-					info(p);
-				}else if(select == 4  ) {
-					break;
-				}
-			}
-		}
-		
-		public static void input(Person p) {
+	public static void main(String[] args) {
+
+		while (true) {
+			System.out.println(" 1 : nhap thong tin ve xe");
+			System.out.println("2 : nhap thong tin ve tau");
+			System.out.println("3 : exit");
+
+			System.out.println("chon thong tin");
 			Scanner sc = new Scanner(System.in);
-			
-			if(p instanceof car) {
-				System.out.println("nhap gia ve");
-				((car)p).setPrice(sc.nextLine());
-				System.out.println("nhap cho ngoi");
-				((car)p).setSlot(sc.nextLine());
-				System.out.println("nhap loai xe");
-				((car)p).setTyle(sc.nextLine());
-			}else if(p instanceof Train) {
-				System.out.println("nhap gia ve");
-				((Train)p).setPrice(sc.nextLine());
-				System.out.println("nhap cho ngoi");
-				((Train)p).setSlot(sc.nextLine());
-				System.out.println("nhap so toa");
-				((Train)p).setSotoa(sc.nextLine());
-	
+			int select = sc.nextInt();
+
+			if (select == 1) {
+				Person p = new car();
+				input(p);
+				info(p);
+				break;
+			} else if (select == 2) {
+				Person p = new Train();
+				input(p);
+				info(p);
+				break;
+			} else if (select == 3) {
+				break;
 			}
 		}
-		
-		public static void info(Person p ) {
-			if(p instanceof car) {
-				
-				System.out.println("gia ve xe " + ((car)p).getPrice() + "cho ngoi " + ((car)p).getSlot() + "loai xe " + ((car)p).getTyle());
-			}else if(p instanceof Train) {
-				
-				System.out.println("gia ve xe " + ((Train)p).getPrice() + " cho ngoi "+ ((Train)p).getSlot() + "so toa " + ((Train)p).getSotoa() );
-			}
+	}
+
+	public static void input(Person p) {
+		Scanner sc = new Scanner(System.in);
+
+		if (p instanceof car) {
+			System.out.println("nhap gia ve");
+			((car) p).setPrice(sc.nextLine());
+			System.out.println("nhap cho ngoi");
+			((car) p).setSlot(sc.nextLine());
+			System.out.println("nhap loai xe");
+			((car) p).setTyle(sc.nextLine());
+		} else if (p instanceof Train) {
+			System.out.println("nhap gia ve");
+			((Train) p).setPrice(sc.nextLine());
+			System.out.println("nhap cho ngoi");
+			((Train) p).setSlot(sc.nextLine());
+			System.out.println("nhap so toa");
+			((Train) p).setSotoa(sc.nextLine());
+
 		}
+	}
+
+	public static void info(Person p) {
+		if (p instanceof car) {
+
+			System.out.println("gia ve xe: " + ((car) p).getPrice() + "cho ngoi: " + ((car) p).getSlot() + "loai xe: "
+					+ ((car) p).getTyle());
+		} else if (p instanceof Train) {
+
+			System.out.println("gia ve xe: " + ((Train) p).getPrice() + " cho ngoi: " + ((Train) p).getSlot()
+					+ "so toa: " + ((Train) p).getSotoa());
+		}
+	}
 }
